8d6e7c19c0b3e7b08f0003e4d735c8e2d6f015ef
creuser <151720755+creuserr@users.noreply.github.com>
2023-12-19 21:42:28 +0800

    update index.js

--- cdn/index.js
+++ cdn/index.js
@@ -2,5 +2,6 @@ export default function handler(request, response) {
   const user = request.query.user;
   const repo = request.query.repo;
   const path = request.query.path;
+  const url = `https://api.github.com/repos/${user}/${repo}/contents/${path}`;
   return response.send(`Hello ${name}!`);
 }
