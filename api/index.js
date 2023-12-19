241bb682c8b46f3b7b71a309a0abd4badfa6b405
creuser <151720755+creuserr@users.noreply.github.com>
2023-12-19 23:52:43 +0800

    update index.js

--- api/index.js
+++ api/index.js
@@ -1,14 +1,14 @@
-export default async function handler(request, response) {
+export default async function(request) {
   var user = request.query.user;
   var repo = request.query.repo;
   var path = request.query.path;
-  if(user == null || repo == null || path == null) return response.status(400).send("Error: incomplete parameter");
+  if(user == null || repo == null || path == null) return new ("Error: incomplete parameter");
   var url = `https://api.github.com/repos/${user}/${repo}/contents/${path}`;
   try {
     var req = await fetch(url);
     var json = await req.json();
-    return response.send(unescape(atob(json.content)));
+    return new Response(unescape(atob(json.content)));
   } catch(e) {
-    return response.status(400).send(`Error: ${e}`);
+    return new Response(`Error: ${e}`);
   }
 }
