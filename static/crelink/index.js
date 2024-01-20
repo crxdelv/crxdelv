cbced2166f365df28a585a7cdf025ad4f3c1d328
creuser <151720755+creuserr@users.noreply.github.com>
2024-01-21 01:59:57 +0800

    update index.js

--- static/crelink/index.js
+++ static/crelink/index.js
@@ -1,7 +1,7 @@
 module.exports = async function(req, res) {
   res.setHeader("Content-Type", "text/html");
   try {
-    var links = await fetch()
+    var links = await fetch("https://crestatic.vercel.app/creuserr/creuserr/static/");
   } catch(e) {
     res.status(400).send("Failed to connect");
   }
