099717b86c9af089a9ba0fb6b5b44f90be91d183
creuser <151720755+creuserr@users.noreply.github.com>
2024-01-21 01:59:24 +0800

    update index.js

--- static/crelink/index.js
+++ static/crelink/index.js
@@ -1,6 +1,8 @@
 module.exports = async function(req, res) {
   res.setHeader("Content-Type", "text/html");
   try {
-    
+    var links = await fetch()
   } catch(e) {
+    res.status(400).send("Failed to connect");
+  }
 }
