482a7f9e85149ba3afb148d6f40d0c368edf48fe
si delv <151720755+creuserr@users.noreply.github.com>
2024-04-19 01:00:30 +0800

    update badge.js

--- badge.js
+++ badge.js
@@ -2,8 +2,8 @@ module.exports = async (req, res) => {
   let f = await fetch("https://creprox.vercel.app/https:/api.github.com/users/creuserr/repos")
   let raw = await f.json()
   let count = raw.length
-  let f = await fetch("https://creprox.vercel.app/https:/api.github.com/users/dlvdls18/repos")
-  let raw = await f.json()
+  f = await fetch("https://creprox.vercel.app/https:/api.github.com/users/dlvdls18/repos")
+  raw = await f.json()
   count += raw.length
   res.setHeader("Access-Control-Allow-Origin", "*")
   res.setHeader("Location", `https://img.shields.io/static/v1?label=Repositories&message=${count}&color=2ea44f&style=for-the-badge&logo=github`)
