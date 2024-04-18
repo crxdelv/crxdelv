ff056d760f75231b6010dfac11b196ccbf16df56
si delv <151720755+creuserr@users.noreply.github.com>
2024-04-19 00:58:57 +0800

    update badge.js

--- badge.js
+++ badge.js
@@ -1,8 +1,12 @@
 module.exports = async (req, res) => {
-  const f = await fetch("https://creprox.vercel.app/https:/api.github.com/users/creuserr/repos")
-  const raw = await f.json()
+  let f = await fetch("https://creprox.vercel.app/https:/api.github.com/users/creuserr/repos")
+  let raw = await f.json()
+  let count = raw.length
+  let f = await fetch("https://creprox.vercel.app/https:/api.github.com/users/dlvdls18/repos")
+  let raw = await f.json()
+  count += raw.length
   res.setHeader("Access-Control-Allow-Origin", "*")
-  res.setHeader("Location", `https://img.shields.io/static/v1?label=Repositories&message=${raw.length}&color=2ea44f&style=for-the-badge&logo=github`)
+  res.setHeader("Location", `https://img.shields.io/static/v1?label=Repositories&message=${count}&color=2ea44f&style=for-the-badge&logo=github`)
   res.statusCode = 301
   res.end()
 }
