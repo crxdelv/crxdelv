cf4d6bedf3ce704a1279df77113b41a623380bc8
si delv <151720755+creuserr@users.noreply.github.com>
2024-06-03 13:52:06 +0800

    update badge.js

--- api/badge.js
+++ api/badge.js
@@ -2,7 +2,7 @@ module.exports = async (req, res) => {
   let f = await fetch("https://api.github.com/users/creuserr/repos")
   let raw = await f.json()
   let count = raw.length
-  f = await fetch("https://https:/api.github.com/users/dlvdls18/repos")
+  f = await fetch("https://api.github.com/users/dlvdls18/repos")
   raw = await f.json()
   count += raw.length
   res.setHeader("Access-Control-Allow-Origin", "*")
