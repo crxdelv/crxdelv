ad1b688bf2a8e818af05634b38556c7678bae981
si delv <151720755+creuserr@users.noreply.github.com>
2024-07-25 00:31:00 +0800

    update badge.js

--- api/badge.js
+++ api/badge.js
@@ -5,6 +5,9 @@ module.exports = async (req, res) => {
   f = await fetch("https://api.github.com/users/dlvdls18/repos")
   raw = await f.json()
   count += raw.length
+  f = await fetch("https://api.github.com/users/dunejp/repos")
+  raw = await f.json()
+  count += raw.length
   res.setHeader("Access-Control-Allow-Origin", "*")
   res.setHeader("Location", `https://img.shields.io/static/v1?label=Repositories&message=${count}&color=blue&style=for-the-badge&logo=github`)
   res.statusCode = 302
